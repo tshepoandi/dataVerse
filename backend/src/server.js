b942163d66f155f9d9344ccd2da2b0c12bc0a83d
milan <tshepoandi>
2024-04-05 17:46:32 +0200

    refactoring

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { connectToDatabase } from './service/mongodb.js'
+// import { connectToDatabase } from './service/mongodb.js'
 import promptRoutes from '../routes/promptRoutes.js'
 dotenv.config();
 
@@ -13,7 +13,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api', promptRoutes);
 
 app.listen(port, () => {
