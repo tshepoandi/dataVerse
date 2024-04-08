b71c31565c9842b81516738f30441a1afaff88c9
milan <tshepoandi>
2024-04-08 16:53:33 +0200

    rollback

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,9 +13,7 @@ connectToDatabase();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: "https://dataverse-client.netlify.app/"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
