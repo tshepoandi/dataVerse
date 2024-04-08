c00b02e06aa16d592ee231735cfa54435978dd07
milan <tshepoandi>
2024-04-08 16:49:50 +0200

    cors origin setting

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,9 @@ connectToDatabase();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://dataverse-client.netlify.app/"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
